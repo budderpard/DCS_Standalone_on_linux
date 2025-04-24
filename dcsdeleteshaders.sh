0d60393f08da080d2bf09333401b643a09f897fb
ChaosRifle <DC3120@gmail.com>
2025-04-24 04:49:24 -0600

    Create dcsdeleteshaders.sh
    added my update shell script to run on patch for shader deletion, in accordance with ED recommendations.

---
+++ dcsdeleteshaders.sh
@@ -0,0 +1,4 @@
+rm -rf '/run/media/chaos/8458f6c5-226e-47b3-84d4-ed25240c780f/games/dcs-world/drive_c/users/chaos/Saved Games/DCS.openbeta/metashaders2/'
+rm -rf '/run/media/chaos/8458f6c5-226e-47b3-84d4-ed25240c780f/games/dcs-world/drive_c/users/chaos/Saved Games/DCS.openbeta/fxo/'
+mkdir '/run/media/chaos/8458f6c5-226e-47b3-84d4-ed25240c780f/games/dcs-world/drive_c/users/chaos/Saved Games/DCS.openbeta/metashaders2/'
+mkdir '/run/media/chaos/8458f6c5-226e-47b3-84d4-ed25240c780f/games/dcs-world/drive_c/users/chaos/Saved Games/DCS.openbeta/fxo/'
